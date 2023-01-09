4efe468462554b03829766c3c619dad55e37768f
VincentCazenave <cazenave@et.esiea.fr>
2023-01-09 14:23:09 +0100

    other modifs

--- reto_front_v2/src/app/app.module.ts
+++ reto_front_v2/src/app/app.module.ts
@@ -6,10 +6,17 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { IonicStorageModule } from '@ionic/storage-angular';
+
+import { CategorieModalComponent } from './categorie-modal/categorie-modal.component';
+import { ChallengeModalComponent } from './challenge-modal/challenge-modal.component';
+
+
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  declarations: [AppComponent, CategorieModalComponent, ChallengeModalComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), IonicStorageModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
